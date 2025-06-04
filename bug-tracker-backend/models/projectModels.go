1ff3c68c169d6372978c1fa76a4e49055470d1e0
Saiyera Rehmina <71667746+srneha24@users.noreply.github.com>
2025-06-04 17:27:58 +0600

    Models Fixed

--- bug-tracker-backend/models/projectModels.go
+++ bug-tracker-backend/models/projectModels.go
@@ -9,7 +9,7 @@ import (
 
 type Project struct {
 	gorm.Model
-	Title       string `json:"title" gorm:"not null"`
+	Title       string `json:"title" gorm:"not null;type:varchar(100)"`
 	Description string `json:"description"`
 	CreatedBy   uint   `json:"created_by" gorm:"not null"`
 	User        User   `json:"-" gorm:"foreignKey:CreatedBy;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // User who created the project
@@ -17,22 +17,22 @@ type Project struct {
 
 type Team struct {
 	gorm.Model
-	ProjectID uint    `json:"project_id" gorm:"not null"`
-	Project   Project `json:"project" gorm:"foreignKey:ProjectID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Project associated with the team
-	UserID    uint    `json:"user_id" gorm:"not null"`
-	User      User    `json:"user" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // User associated with the team
+	ProjectID uint    `json:"project_id" gorm:"not null;uniqueIndex:idx_project_user"`                                   // Unique index to prevent duplicate team members in the same project
+	Project   Project `json:"-" gorm:"foreignKey:ProjectID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Project associated with the team
+	UserID    uint    `json:"user_id" gorm:"not null;uniqueIndex:idx_project_user"`                                      // Unique index to prevent duplicate team members in the same project
+	User      User    `json:"-" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`    // User associated with the team
 	Role      string  `json:"role" gorm:"not null"`
 }
 
 type Bug struct {
 	gorm.Model
-	Title        string         `json:"title" gorm:"not null"`
+	Title        string         `json:"title" gorm:"not null;type:varchar(100)"`
 	Description  string         `json:"description"`
-	Tags         pq.StringArray `json:"tags" gorm:"type:text[]"`
+	Tags         pq.StringArray `json:"tags" gorm:"type:varchar[]"`
 	Deadline     time.Time      `json:"deadline" gorm:"not null"`
 	Priority     uint           `json:"priority" gorm:"not null"` // 1: High, 2: Medium, 3: Low
 	AssignedTo   uint           `json:"assigned_to" gorm:"not null"`
-	AssignedUser User           `json:"assigned_user" gorm:"foreignKey:AssignedTo;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // User to whom the bug is assigned
+	AssignedUser User           `json:"-" gorm:"foreignKey:AssignedTo;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // User to whom the bug is assigned
 	ProjectID    uint           `json:"project_id" gorm:"not null"`
-	Project      Project        `json:"project" gorm:"foreignKey:ProjectID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Project associated with the bug
+	Project      Project        `json:"-" gorm:"foreignKey:ProjectID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"` // Project associated with the bug
 }
--- bug-tracker-backend/models/userModels.go
+++ bug-tracker-backend/models/userModels.go
@@ -4,8 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string    `json:"username" gorm:"unique;not null"`
-	Email    string    `json:"email" gorm:"unique;not null"`
+	Name     string    `json:"name" gorm:"not null;type:varchar(100)"`
+	Username string    `json:"username" gorm:"unique;not null;type:varchar(100)"`
+	Email    string    `json:"email" gorm:"unique;not null;type:varchar(100)"`
 	Password string    `json:"password" gorm:"not null"`
 	Projects []Project `json:"projects" gorm:"foreignKey:CreatedBy;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"` // Projects created by the user
 	Teams    []Team    `json:"teams" gorm:"foreignKey:UserID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`        // Teams the user is part of
