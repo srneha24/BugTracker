5e20a7a1d5c76a35b93c07764c0bc5797a90cb20
brittanyrobarge <147116052+brittanyrobarge@users.noreply.github.com>
2025-06-20 13:21:52 -0400

    Save progress before switching branches

--- bug-tracker-backend/main.go
+++ bug-tracker-backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-contrib/cors"
 
@@ -17,7 +18,14 @@ func init() {
 func main() {
 	router := gin.Default()
 
-	router.Use(cors.Default())
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Middlewares
 	router.Use(middlewares.StandardResponseMiddleware)
